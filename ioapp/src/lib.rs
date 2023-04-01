6ace66acdf34c0d3948c4d7b8e71120eb085b7b1
TakiMoysha <mpwema782@gmail.com>
2023-04-01 22:57:13 +0300

    added search_case_insensitive

---
+++ ioapp/src/lib.rs
@@ -0,0 +1,81 @@
+use std::fs;
+use std::env;
+use std::error::Error;
+
+#[derive(Default)]
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive\nDuct tape\nEnd.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "Rust:\nsafe, fast, productive\ntrust me.\nEnd.";
+
+        assert_eq!(vec!["Rust:", "trust me."], search_case_insensitive(query, contents));
+    }
+}
--- ioapp/src/main.rs
+++ ioapp/src/main.rs
@@ -1,29 +1,7 @@
-use std::fs;
 use std::env;
 use std::process;
-use std::error::Error;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        Ok(Config { query: args[1].clone(), file_path: args[2].clone() })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text\n {contents}");
-
-    Ok(())
-}
+use ioapp::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,10 +11,7 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for <{}>", config.query);
-    println!("In file <{}>", config.file_path);
-
-    if let Err(e) = run(config) {
+    if let Err(e) = ioapp::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
