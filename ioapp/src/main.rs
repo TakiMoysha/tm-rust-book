e1a0e6f9af65e96a29c4f222dc06e569cb1b078a
TakiMoysha <mpwema782@gmail.com>
2023-04-01 22:13:35 +0300

    added ai application

---
+++ ioapp/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::env;
+use std::process;
+use std::error::Error;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        Ok(Config { query: args[1].clone(), file_path: args[2].clone() })
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text\n {contents}");
+
+    Ok(())
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for <{}>", config.query);
+    println!("In file <{}>", config.file_path);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
+}
