da7dd5ff0adf51d9ac1fdb02d6cef8446802061e
TakiMoysha <mpwema782@gmail.com>
2023-11-13 01:01:34 +0100

    some tinkering

--- random-projects/bevy_gpu/src/main.rs
+++ random-projects/bevy_gpu/src/main.rs
@@ -1,12 +1,46 @@
+use bevy::{prelude::*, window::WindowResized};
+
+fn make_visible(mut window: Query<&mut Window>) {
+    window.single_mut().visible = true;
+}
+fn on_resize_system(mut resize_reader: EventReader<WindowResized>) {
+    for event in resize_reader.read() {
+        println!("window resized: {:?}", event);
+    }
+}
+
+fn main() {
+    let app_primary_window = Window {
+        title: "TakiApp".into(),
+        resolution: (460., 370.).into(),
+        visible: false,
+        ..default()
+    };
+    let overwrite_defaul_plugin = WindowPlugin {
+        primary_window: Some(app_primary_window),
+        ..default()
+    };
+    App::new()
+        .add_systems(Startup, make_visible)
+        .add_systems(Update, on_resize_system)
+        .add_plugins((
+            DefaultPlugins.set(overwrite_defaul_plugin),
+            particles::CorePlugins,
+        ))
+        .run();
+}
+
 pub mod particles {
     use bevy::{
         input::{mouse::MouseButtonInput, ButtonState},
         prelude::*,
+        sprite::*,
         window::PrimaryWindow,
     };
 
+    // ################## COMPONENTS
     #[derive(Component, Clone)]
-    pub struct Particle {
+    pub struct FireworkParticle {
         velocity: Vec2,
         position: Vec2,
         // lifetime: Option<f32>, // or Timer
@@ -14,7 +48,7 @@ pub mod particles {
     }
 
     #[derive(Component, Debug)]
-    pub struct FireworksParticleSpawner {
+    pub struct FireworkParticleSpawner {
         rate: f32,
         amount_per_burst: usize,
         lifetime: f32,
@@ -24,34 +58,15 @@ pub mod particles {
     #[derive(Component, Debug)]
     pub struct Ground;
 
-    // DOMAIN
+    // ################## DOMAIN
     fn spawn_fireworks(
         commands: &mut Commands,
         pos: Vec2,
-        mut spawners: Query<(Entity, &mut FireworksParticleSpawner)>,
-        time: Res<Time>,
     ) {
-        for (ent, mut spawner) in spawners.iter_mut() {
-            println!("spawner: {:?}", spawner);
-            spawner.timer.tick(time.delta());
-            println!("timer: {:?}", spawner.timer);
-            // if spawner.timer.just_finished() {
-            // println!("spawning");
-            // for index in 0..spawner.amount_per_burst {
-            //     let particle = Particle {
-            //         velocity: Vec2::Y * -1.,
-            //         position: pos,
-            //         lifetime: Timer::from_seconds(spawner.lifetime, TimerMode::Once),
-            //     };
-            //     let particle = commands.spawn_empty().insert(particle).id();
-            //     let mut sprite = SpriteBundle::default();
-            //     // commands.entity(ent).add_child(particle);
-            // }
-            // }
-        }
+
     }
 
-    // PLUGINS
+    // ################## PLUGINS
     fn setup(
         mut commands: Commands,
         mut meshes: ResMut<Assets<Mesh>>,
@@ -76,8 +91,20 @@ pub mod particles {
         );
         commands.spawn((
             PbrBundle {
-                mesh: sphere_handle.clone(),
+                mesh: sphere_handle,
                 material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+                ..default()
+            },
+            Ground,
+        ));
+        commands.spawn((
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(shape::Plane {
+                    size: 128.0,
+                    subdivisions: 1,
+                })),
+                material: materials.add(Color::WHITE.into()),
+                transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
                 ..default()
             },
             Ground,
@@ -115,7 +142,7 @@ pub mod particles {
                 ButtonState::Pressed => match p_window.single().cursor_position() {
                     Some(pos) => {
                         println!("Mouse left button pressed at {:?}", pos);
-                        // spawn_fireworks(&mut commands, pos);
+                        spawn_fireworks(&mut commands, pos);
                     }
                     None => {
                         println!("Mouse left button pressed, but no cursor position available");
@@ -126,65 +153,12 @@ pub mod particles {
         }
     }
 
-    fn draw_cursor(
-        camera_query: Query<(&Camera, &GlobalTransform)>,
-        ground_query: Query<&GlobalTransform, With<Ground>>,
-        windows: Query<&Window>,
-        mut gizmos: Gizmos,
-    ) {
-        let (camera, camera_transform) = camera_query.single();
-        let ground = ground_query.single();
-        let Some(cursor_position) = windows.single().cursor_position() else {
-            return;
-        };
-        let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
-            return;
-        };
-        let Some(distance) = ray.intersect_plane(ground.translation(), ground.up()) else {
-            return;
-        };
-        let point = ray.get_point(distance);
-        gizmos.circle(point + ground.up() * 3., ground.up(), 0.1, Color::WHITE);
-    }
-
-    // SUPPORT
+    // ################## SUPPORT
     pub struct CorePlugins;
     impl Plugin for CorePlugins {
         fn build(&self, app: &mut App) {
             app.add_systems(Startup, setup)
-                .add_systems(Update, (mouse_handler, update, draw_cursor));
+                .add_systems(Update, (mouse_handler, update));
         }
     }
-}
-
-use bevy::{prelude::*, window::WindowResized};
-
-fn make_visible(mut window: Query<&mut Window>) {
-    window.single_mut().visible = true;
-}
-fn on_resize_system(mut resize_reader: EventReader<WindowResized>) {
-    for event in resize_reader.read() {
-        println!("window resized: {:?}", event);
-    }
-}
-
-fn main() {
-    let app_primary_window = Window {
-        title: "TakiApp".into(),
-        resolution: (460., 370.).into(),
-        visible: false,
-        ..default()
-    };
-    let overwrite_defaul_plugin = WindowPlugin {
-        primary_window: Some(app_primary_window),
-        ..default()
-    };
-    App::new()
-        .add_systems(Startup, make_visible)
-        .add_systems(Update, on_resize_system)
-        .add_plugins((
-            DefaultPlugins.set(overwrite_defaul_plugin),
-            particles::CorePlugins,
-        ))
-        .run();
 }
