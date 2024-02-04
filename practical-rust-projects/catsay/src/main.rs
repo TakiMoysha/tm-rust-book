8d3b19a902e428b41b44e9927893fd417fbfa0e2
TakiMoysha <mpwema782@gmail.com>
2024-02-04 01:21:43 +0100

    update

--- practical-rust-projects/catsay/src/main.rs
+++ practical-rust-projects/catsay/src/main.rs
@@ -73,3 +73,19 @@ fn main() {
     )
     .unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::process::Command;
+    use assert_cmd::prelude::*;
+
+    #[test]
+    fn run_with_defaults() {
+        Command::cargo_bin("catsay")
+            .expect("binary exists")
+            .assert()
+            .success(); 
+    }
+}
