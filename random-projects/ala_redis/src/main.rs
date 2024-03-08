6175e1e5b9b114e65806661cad8f610e9e2bdf7b
TakiMoysha <mpwema782@gmail.com>
2024-03-08 23:47:38 +0100

    updated, return to working state

--- random-projects/ala_redis/src/main.rs
+++ random-projects/ala_redis/src/main.rs
@@ -1,85 +1,104 @@
 #![allow(warnings)] // hide warnings
 
-use anyhow::Result;
+use anyhow::{Context, Result};
+use clap::Parser;
 use log::{debug, info};
 use std::net::SocketAddr;
-use std::sync::{mpsc, Mutex, Arc};
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread::sleep;
 use std::time::SystemTime;
-use tokio::time::{sleep_until, Instant, Duration};
+use tokio::time::{sleep_until, Duration, Instant};
+use chrono::DateTime;
+use chrono::Local;
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufStream};
 use tokio::net::{TcpListener, TcpStream};
-use tokio::{signal};
+use tokio::signal;
 // use tokio::sync::mpsc;
 
-mod runtime;
-
-const ADDR: &str = "127.0.0.1:7878";
-
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct ServerOpts {
-    address: String,
+    #[clap(short = 'b', long = "bind", default_value = "127.0.0.1:6379")]
+    address: Option<String>,
 }
 
+// deprecated
 struct ServerRuntime {
     listener: TcpListener,
     tx: mpsc::Sender<anyhow::Error>,
     rx: mpsc::Receiver<anyhow::Error>,
-    runtime: tokio::runtime::Runtime,
     config: ServerOpts,
 }
 
-async fn run_server() -> Result<ServerRuntime> {
-    let opts = ServerOpts {
-        address: ADDR.to_string(),
-    };
+async fn run_server(opts: ServerOpts) -> Result<ServerRuntime> {
+    let address = opts.address.as_ref().expect("Address must be provided");
     let (tx, rx) = mpsc::channel::<anyhow::Error>();
-    let listener = TcpListener::bind(ADDR).await.unwrap();
+    let listener = TcpListener::bind(address)
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to bind server, by reason: {}", e))?;
     let server: ServerRuntime = ServerRuntime {
         listener,
         tx,
         rx,
-        runtime: tokio::runtime::Runtime::new().unwrap(),
-        config: opts
+        config: opts,
     };
 
     Ok(server)
 }
 
+async fn handle_request(mut stream: &mut BufStream<TcpStream>) -> Result<()> {
+    let contents = "{\"hello\": \"world\"}";
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
 
-async fn stream_process(mut stream: TcpStream) {
-    let mut buf = [0; 1024];
-    stream.read_to_end(&mut buf).unwrap();
-
-
+    stream.write_all(response.as_bytes()).await?;
+    stream.flush().await?;
+    Ok(())
 }
 
-
 #[tokio::main]
-async fn main() {
-    let server = run_server().await.unwrap();
-    // sleep 2 seconds
-    debug!("time:{} Server running on {}", 0, server.config.address);
+async fn main() -> Result<()> {
+    let server_opts = ServerOpts::parse();
+    let server = run_server(server_opts).await?;
+    debug!("time:{} Server running on {:?}", 0, server.config);
 
+    println!("Listening on {:?}", server.listener.local_addr().unwrap());
     loop {
-        let (stream, addr) = server.listener.accept().await.unwrap();
-        let mut stream = socket.0;
-        
+        let (socket, _) = server
+            .listener
+            .accept()
+            .await
+            .map_err(|e| anyhow::anyhow!("Failed to accept connection, by reason: {}", e))?;
+        let mut stream = BufStream::new(socket);
+
         tokio::spawn(async move {
-            stream_process(state, stream, addr).await;
+            let time: DateTime<Local> = SystemTime::now().into();
+            info!(
+                "[{}] <{}> Connection",
+                time, 
+                stream.get_ref().peer_addr().unwrap(), 
+            );
+
+            handle_request(&mut stream).await.map_err(|e| {
+                anyhow::anyhow!("Failed to handle request, by reason: {}", e)
+            })
         });
-
-        // async move {
-        //     let mut buffer = [0, 1024];
-        //     let mut buffer = Vec::new();
-        //     let mut stream = socket.0;
-            // let request = stream.read_to_end(&mut buffer);
-
-            // let _ = stream.read(&mut buffer).await.unwrap();
-            // let mut stream = socket.0;
-            // let mut buf = [0; 512]; 
-        // });
+        //
+        //     // async move {
+        //     //     let mut buffer = [0, 1024];
+        //     //     let mut buffer = Vec::new();
+        //     //     let mut stream = socket.0;
+        //     // let request = stream.read_to_end(&mut buffer);
+        //
+        //     // let _ = stream.read(&mut buffer).await.unwrap();
+        //     // let mut stream = socket.0;
+        //     // let mut buf = [0; 512];
+        //     // });
     }
 
-    // println!("Listening on {}", listener.local_addr().unwrap());
+    Ok(())
 }
