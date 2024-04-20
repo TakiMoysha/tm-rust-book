644eb0fb975f169bff40e58a76fbfd242996baa4
TakiMoysha <mpwema782@gmail.com>
2024-04-20 21:28:24 +0200

    updated, text render

--- practice/wasm-roguelike/src/main.rs
+++ practice/wasm-roguelike/src/main.rs
@@ -10,12 +10,13 @@ pub mod lib {
         geom::{Circle, Rectangle, Vector},
         graphics::{
             blend::{BlendChannel, BlendFactor, BlendFunction, BlendInput, BlendMode},
-            Color, Element, Graphics, Image, Mesh, PixelFormat, Surface, Vertex,
+            Color, Element, FontRenderer, Graphics, Image, Mesh, PixelFormat, Surface, VectorFont,
+            Vertex,
         },
-        Input, Result, Settings, Window,
+        Input, Result, Settings, Timer, Window,
     };
 
-    fn draw_atlas(gfx: &mut Graphics, image: Image) {
+    fn draw_atlas(gfx: &mut Graphics, image: &Image) {
         gfx.clear(Color::WHITE);
         gfx.set_blend_mode(Some(BlendMode {
             equation: Default::default(),
@@ -46,18 +47,31 @@ pub mod lib {
             },
             global_color: [0.0; 4],
         }));
-
-        let region = Rectangle::new(Vector::new(0.0, 0.0), Vector::new(100.0, 40.0));
-        gfx.draw_image(&image, region);
-        gfx.fill_rect(&region, Color::RED.with_alpha(0.1));
     }
 
     pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
-        let img_atlas = Image::load(&gfx, "assets/atlas.png").await?;
+        let asset_atlas = Image::load(&gfx, "assets/atlas.png").await?;
+        let asset_font = VectorFont::load("assets/amethysta.ttf").await?;
 
-        draw_atlas(&mut gfx, img_atlas);
+        // draw atlas
+        // draw_atlas(&mut gfx, &asset_atlas);
+
+        // draw text
+        let font_size = 42.0;
+        let mut font = asset_font.to_renderer(&gfx, font_size)?;
+        let font_color = Color::from_hex("#FAAA33");
+        font.draw(
+            &mut gfx,
+            "Hello world\nQuicksilver!",
+            font_color,
+            Vector::new(32.0, 32.0),
+        )?;
 
         gfx.present(&window)?;
+
+        // let mut update_timer = Timer::time_per_second(30.0);
+        // let mut draw_timer = Timer::time_per_second(60.0);
+        // let mut rect = Rectangle::new(Vector::new(0.0, 80.0), Vector::new(16.0, 16.0));
 
         loop {
             while let Some(_) = input.next_event().await {}
