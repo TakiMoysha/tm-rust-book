7dffb0741fcd4e27bc28262aa3be5ccd1c6e86e0
TakiMoysha <mpwema782@gmail.com>
2024-04-21 16:05:18 +0200

    updated, events

--- practice/wasm-roguelike/src/main.rs
+++ practice/wasm-roguelike/src/main.rs
@@ -13,40 +13,41 @@ pub mod lib {
             Color, Element, FontRenderer, Graphics, Image, Mesh, PixelFormat, Surface, VectorFont,
             Vertex,
         },
+        input::{Event, Key},
         Input, Result, Settings, Timer, Window,
     };
 
     fn draw_atlas(gfx: &mut Graphics, image: &Image) {
         gfx.clear(Color::WHITE);
-        gfx.set_blend_mode(Some(BlendMode {
-            equation: Default::default(),
-            function: BlendFunction::Same {
-                source: BlendFactor::Color {
-                    input: BlendInput::Source,
-                    channel: BlendChannel::Alpha,
-                    is_inverse: false,
-                },
-                destination: BlendFactor::Color {
-                    input: BlendInput::Source,
-                    channel: BlendChannel::Alpha,
-                    is_inverse: true,
-                },
-            },
-            global_color: [0.0; 4],
-        }));
+        // gfx.set_blend_mode(Some(BlendMode {
+        //     equation: Default::default(),
+        //     function: BlendFunction::Same {
+        //         source: BlendFactor::Color {
+        //             input: BlendInput::Source,
+        //             channel: BlendChannel::Alpha,
+        //             is_inverse: false,
+        //         },
+        //         destination: BlendFactor::Color {
+        //             input: BlendInput::Source,
+        //             channel: BlendChannel::Alpha,
+        //             is_inverse: true,
+        //         },
+        //     },
+        //     global_color: [0.0; 4],
+        // }));
 
         let overlay_region = Rectangle::new(Vector::new(0.0, 0.0), image.size());
         gfx.draw_image(&image, overlay_region);
         gfx.fill_rect(&overlay_region, Color::BLUE.with_alpha(0.3));
 
-        gfx.set_blend_mode(Some(BlendMode {
-            equation: Default::default(),
-            function: BlendFunction::Same {
-                source: BlendFactor::One,
-                destination: BlendFactor::Zero,
-            },
-            global_color: [0.0; 4],
-        }));
+        // gfx.set_blend_mode(Some(BlendMode {
+        //     equation: Default::default(),
+        //     function: BlendFunction::Same {
+        //         source: BlendFactor::One,
+        //         destination: BlendFactor::Zero,
+        //     },
+        //     global_color: [0.0; 4],
+        // }));
     }
 
     pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
@@ -54,7 +55,7 @@ pub mod lib {
         let asset_font = VectorFont::load("assets/amethysta.ttf").await?;
 
         // draw atlas
-        // draw_atlas(&mut gfx, &asset_atlas);
+        draw_atlas(&mut gfx, &asset_atlas);
 
         // draw text
         let font_size = 42.0;
@@ -66,16 +67,45 @@ pub mod lib {
             font_color,
             Vector::new(32.0, 32.0),
         )?;
-
         gfx.present(&window)?;
 
         // let mut update_timer = Timer::time_per_second(30.0);
         // let mut draw_timer = Timer::time_per_second(60.0);
         // let mut rect = Rectangle::new(Vector::new(0.0, 80.0), Vector::new(16.0, 16.0));
 
-        loop {
-            while let Some(_) = input.next_event().await {}
+        let mut square_position = Vector::new(300.0, 300.0);
+        let mut my_string = String::new();
+        let mut is_running = true;
+
+        while is_running {
+            while let Some(event) = input.next_event().await {
+                match event {
+                    Event::KeyboardInput(input_event) => {
+                        if input_event.key() == Key::Escape {
+                            is_running = false;
+                        } else if input_event.key() == Key::Back {
+                            my_string.pop();
+                        }
+                    }
+                    Event::ReceivedCharacter(c) => {
+                        let chr = c.character();
+                        if !chr.is_control() { my_string.push(chr); }
+                    }
+                    _ => {}
+                }
+            }
+
+            // if input.key_down(Key::H) {}
+            // if input.key_down(Key::J) {}
+            // if input.key_down(Key::K) {}
+            // if input.key_down(Key::L) {}
+
+            // gfx.clear(Color::WHITE);
+            font.draw_wrapping(&mut gfx, &my_string, Some(500.0), Color::BLACK, Vector::new(100.0, 100.0))?;
+            gfx.present(&window)?;
         }
+
+        Ok(())
     }
 }
 
