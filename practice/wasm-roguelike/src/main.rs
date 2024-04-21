6f69877297ca6314110aae303f163003d0384ce1
TakiMoysha <mpwema782@gmail.com>
2024-04-21 16:52:02 +0200

    updated, loading screen

--- practice/wasm-roguelike/src/main.rs
+++ practice/wasm-roguelike/src/main.rs
@@ -5,20 +5,23 @@ use quicksilver::{
     Input, Result, Settings, Window,
 };
 
+const DEFAULT_WINDOW_SIZE: Vector = Vector { x: 800.0, y: 600.0 };
+
 pub mod lib {
+    use std::{thread::sleep, time::Duration};
+
     use quicksilver::{
         geom::{Circle, Rectangle, Vector},
         graphics::{
             blend::{BlendChannel, BlendFactor, BlendFunction, BlendInput, BlendMode},
-            Color, Element, FontRenderer, Graphics, Image, Mesh, PixelFormat, Surface, VectorFont,
-            Vertex,
+            Color, Element, FontRenderer, Graphics, Image, Mesh, PixelFormat, ResizeHandler,
+            Surface, VectorFont, Vertex,
         },
         input::{Event, Key},
         Input, Result, Settings, Timer, Window,
     };
 
     fn draw_atlas(gfx: &mut Graphics, image: &Image) {
-        gfx.clear(Color::WHITE);
         // gfx.set_blend_mode(Some(BlendMode {
         //     equation: Default::default(),
         //     function: BlendFunction::Same {
@@ -51,11 +54,18 @@ pub mod lib {
     }
 
     pub async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+        let STEPS = 2;
+
+        for i in 0..STEPS {
+            draw_loader(&window, &mut gfx, i, STEPS);
+            sleep(Duration::from_secs(1));
+        }
+
         let asset_atlas = Image::load(&gfx, "assets/atlas.png").await?;
-        let asset_font = VectorFont::load("assets/amethysta.ttf").await?;
+        let asset_font = VectorFont::load("assets/VividSans.ttf").await?;
 
         // draw atlas
-        draw_atlas(&mut gfx, &asset_atlas);
+        // draw_atlas(&mut gfx, &asset_atlas);
 
         // draw text
         let font_size = 42.0;
@@ -67,29 +77,65 @@ pub mod lib {
             font_color,
             Vector::new(32.0, 32.0),
         )?;
-        gfx.present(&window)?;
 
+        // resize
+        let vertices = {
+            let top = Vertex {
+                pos: Vector::new(400.0, 200.0),
+                uv: None,
+                color: Color::RED.with_alpha(0.2),
+            };
+            let left = Vertex {
+                pos: Vector::new(200.0, 400.0),
+                uv: None,
+                color: Color::GREEN.with_alpha(0.2),
+            };
+            let right = Vertex {
+                pos: Vector::new(600.0, 400.0),
+                uv: None,
+                color: Color::BLUE.with_alpha(0.2),
+            };
+            vec![top, left, right]
+        };
+        let elements = vec![Element::Triangle([0, 1, 2])];
+        let mesh = Mesh {
+            vertices,
+            elements,
+            image: Some(asset_atlas),
+        };
+        let resize_handler = ResizeHandler::Fit {
+            aspect_width: 4.0,
+            aspect_height: 3.0,
+        };
+        gfx.set_resize_handler(resize_handler);
+
+        // timer
         // let mut update_timer = Timer::time_per_second(30.0);
         // let mut draw_timer = Timer::time_per_second(60.0);
         // let mut rect = Rectangle::new(Vector::new(0.0, 80.0), Vector::new(16.0, 16.0));
 
+        // events
         let mut square_position = Vector::new(300.0, 300.0);
         let mut my_string = String::new();
         let mut is_running = true;
 
         while is_running {
+            gfx.clear(Color::WHITE.with_alpha(0.8));
+
             while let Some(event) = input.next_event().await {
                 match event {
                     Event::KeyboardInput(input_event) => {
                         if input_event.key() == Key::Escape {
                             is_running = false;
-                        } else if input_event.key() == Key::Back {
+                        } else if input_event.key() == Key::Back && input_event.is_down() {
                             my_string.pop();
                         }
                     }
                     Event::ReceivedCharacter(c) => {
                         let chr = c.character();
-                        if !chr.is_control() { my_string.push(chr); }
+                        if !chr.is_control() {
+                            my_string.push(chr);
+                        }
                     }
                     _ => {}
                 }
@@ -101,19 +147,48 @@ pub mod lib {
             // if input.key_down(Key::L) {}
 
             // gfx.clear(Color::WHITE);
-            font.draw_wrapping(&mut gfx, &my_string, Some(500.0), Color::BLACK, Vector::new(100.0, 100.0))?;
+            font.draw_wrapping(
+                &mut gfx,
+                &my_string,
+                Some(1200.0),
+                Color::BLACK,
+                Vector::new(100.0, 100.0),
+            )?;
+            gfx.draw_mesh(&mesh);
             gfx.present(&window)?;
         }
 
         Ok(())
     }
+
+    fn draw_loader(
+        window: &Window,
+        gfx: &mut Graphics,
+        progress: usize,
+        total: usize,
+    ) -> Result<()> {
+        gfx.clear(Color::BLACK);
+        gfx.fill_rect(
+            &Rectangle::new(Vector::new(50.0, 500.0), Vector::new(700.0, 25.0)),
+            Color::YELLOW,
+        );
+
+        let width = 700.0 * progress as f32 / total as f32;
+        gfx.fill_rect(
+            &Rectangle::new(Vector::new(50.0, 500.0), Vector::new(width, 25.0)),
+            Color::BLUE,
+        );
+
+        gfx.present(&window)?;
+        Ok(())
+    }
 }
 
 fn main() -> Result<()> {
-    // let font = "amethysta.ttf";
     let settings = Settings {
         title: "TakiRouge",
-        size: Vector::new(1200.0, 600.0),
+        size: DEFAULT_WINDOW_SIZE,
+        // resizable: true,
         ..Settings::default()
     };
     run(settings, lib::app)
