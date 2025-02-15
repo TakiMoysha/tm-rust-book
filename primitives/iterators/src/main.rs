1ef45b11433fd19aa886c85ba944ad32d40fe88b
TakiMoysha <mpwema782@gmail.com>
2025-02-15 01:28:38 +0100

    base iterator

--- primitives/iterators/src/main.rs
+++ primitives/iterators/src/main.rs
@@ -1,13 +1,40 @@
-use std::iter::{ Iterator, IntoIterator };
+mod base_iterators {
+    use std::iter::{IntoIterator, Iterator};
+
+    pub struct Counter {
+        count: u32,
+        limit: u32,
+    }
+
+    impl Iterator for Counter {
+        type Item = u32;
+
+        fn next(&mut self) -> Option<u32> {
+            if self.count < self.limit {
+                self.count += 1;
+                return Some(self.count);
+            }
+            None
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn should_work_stdlib_iterator_trait() {
+            let mut counter = Counter { count: 0, limit: 5 };
+            assert_eq!(counter.next(), Some(1));
+            assert_eq!(counter.next(), Some(2));
+            assert_eq!(counter.next(), Some(3));
+            assert_eq!(counter.next(), Some(4));
+            assert_eq!(counter.next(), Some(5));
+            assert_eq!(counter.next(), None);
+        }
+    }
+}
 
 fn main() {
     println!("Hello, world!");
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn should_work_stdlib_iterator_trait() {}
 }
