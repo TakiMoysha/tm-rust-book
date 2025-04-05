987b757ab9501a0910943db547302890d8c79198
TakiMoysha <takimoysha@users.noreply.github.com>
2025-04-05 05:10:07 +0200

    demo-medium-server: updated

--- practice/demo-medium-server/services/tower/examples/tower-service-alone.rs
+++ practice/demo-medium-server/services/tower/examples/tower-service-alone.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("tower-service-alone");
+use std::pin::Pin;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    println!("Test");
+    Ok(())
 }
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct Service;
