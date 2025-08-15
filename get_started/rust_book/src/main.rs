0cb26aacef854f018023ce683f483c9e469e5691
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-08-16 01:01:04 +0200

    updated deps, refactored rustget_started

--- get_started/rust_book/src/main.rs
+++ get_started/rust_book/src/main.rs
@@ -1,19 +1,17 @@
-mod os;
+mod articles;
+mod codewars;
+mod data_structure;
+mod garden;
 mod ioapp;
 mod offtop;
+mod os;
 mod random;
-mod garden;
-mod traits;
-mod strings;
-mod codewars;
-mod articles;
 mod rust_book;
-mod data_structure;
+mod strings;
+mod traits;
 
 // mod types;
-mod types {
-
-}
+mod types {}
 
 #[warn(unused_must_use)]
 fn main() {
--- get_started/samples/adder/src/main.rs
+++
@@ -1,8 +0,0 @@
-use add_one;
-
-fn main() {
-    let num = 10;
-    println!("hello, world {num} plus one is {}!", add_one::add_one(num));
-    println!("How check treesitter is work?");
-}
-
