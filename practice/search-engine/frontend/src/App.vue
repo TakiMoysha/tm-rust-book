41890a6e513e3eb35413d874c61ae832d4ac5507
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2025-12-16 22:04:56 +0100

    init search-engine project

---
+++ practice/search-engine/frontend/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import * as SearchEngine from "search-engine";
+</script>
+
+<template>
+  <div class="content">
+    <h1>Search Engine</h1>
+    <p>Text about how working it.</p>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  min-height: 100vh;
+  line-height: 1.1;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.content h1 {
+  font-size: 3.6rem;
+  font-weight: 700;
+}
+
+.content p {
+  font-size: 1.2rem;
+  font-weight: 400;
+  opacity: 0.5;
+}
+</style>
---
+++ practice/search-engine/src/lib.rs
@@ -0,0 +1,21 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+fn run() {
+    unsafe {
+        alert("Hello, world!");
+    }
+}
+
+#[wasm_bindgen]
+unsafe extern "C" {
+    unsafe fn alert(s: &str);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn target() {}
+}
