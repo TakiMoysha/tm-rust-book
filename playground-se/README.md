433319a723b1d23ef51b44cfb5dbd84aec46fac2
TakiMoysha <36836047+TakiMoysha@users.noreply.github.com>
2026-05-10 08:39:43 +0200

    init playground for space engineers

---
+++ playground-se/README.md
@@ -0,0 +1,12 @@
+
+## TODO
+
+XML (.sbc)
+Чтение XML использует стандартный .NET XmlSerializer SpaceEngineersAPI.cs:198-203 . Это можно портировать, но вам понадобятся схемы классов MyObjectBuilder_* из SpaceEngineers.ObjectBuilders.dll.
+
+Protobuf (.sbcPB)
+Использует MyObjectBuilderSerializer.DeserializePB из VRage SpaceEngineersAPI.cs:78 . Формат Protobuf, но схема (.proto файлы) не включена в репозиторий — она компилирована в DLL.
+
+.mwm файлы
+Использует MyModel.LoadModelData() из VRage.Import . Это проприетарный бинарный формат Keen Software House без публичной спецификации.
+
---
+++ playground-se/src/main.rs
@@ -0,0 +1,40 @@
+use anyhow;
+use std::{env, fs, path::Path};
+
+use quick_xml::{events::Event, reader::Reader};
+
+
+struct SbcObject {
+
+}
+
+fn try_read_se_file(filename: &Path) -> anyhow::Result<()> {
+    todo!()
+}
+
+fn main() {
+    let test_path = env::var("TEST_DIR").expect("TEST_DIR must be set");
+
+    let data_dir = Path::new(&test_path).join("Data");
+
+    println!("TEST_DIR {}: {}", data_dir.exists(), data_dir.display());
+
+    // loading data-files
+    let data_files: Vec<_> = fs::read_dir(data_dir)
+        .expect("failed to read data dir")
+        .filter_map(|entry| {
+            if let Ok(e) = entry {
+                if e.path().is_file() && e.path().extension().is_some_and(|ext| ext == "sbc") {
+                    Some(e)
+                } else {
+                    None
+                }
+            } else {
+                eprintln!("failed to read entry: {}", entry.unwrap_err());
+                None
+            }
+        })
+        .collect();
+
+    println!("files: {:?}", data_files);
+}
